5017dea8c92ea4d601287fc9918b989221bcd3d4
Bipin Maharjan <21078512+iambpn@users.noreply.github.com>
2021-07-22 22:26:41 +0545

    Apis added for backend

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -1,5 +1,6 @@
-const express  = require("express");
+const express = require("express");
 const cors = require("cors");
+const {sequelize, tasks} = require("./models");
 
 let app = express();
 let port = 3000;
@@ -7,20 +8,57 @@ let port = 3000;
 app.use(cors())
 app.use(express.json());
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Need to Link Dist index here");
 })
 
-app.post("/api/addTodo",(req,res)=>{
+app.get("/api/todo", async (req, res) => {
+    try {
+        const allTasks = await tasks.findAll();
+        res.json(allTasks);
+    } catch (e) {
+        res.status(500).json({message: e.message || "Failed to fetch data from server."});
+    }
+});
+
+app.post("/api/addTodo", async (req, res) => {
     try {
         const {content} = req.body;
-        res.json({content, id: 10,completed:false})
-    }
-    catch{
-        res.status(500).json({message:"Server Error"});
+        const task = await tasks.create({content})
+        res.json(task)
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({message: e.message || "Server Error"});
     }
 })
 
-let server = app.listen(port,()=>{
-    console.log("Express Server Running on port :"+port);
+app.put("/api/todo/completed", async (req, res) => {
+    try {
+        const {id} = req.body;
+        const task = await tasks.findOne({where: {uuid: id}});
+        task.completed = !task.completed;
+        await task.save();
+        res.json({message: "Todo Updated."});
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({message: e.message || "Error while updating database."});
+    }
+});
+
+app.delete("/api/todo", async (req, res) => {
+    try {
+        const {id} = req.body;
+        const task = await tasks.findOne({where: {uuid: id}});
+        await task.destroy();
+        res.json({message: "Todo Deleted."});
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({message: e.message || "Error while updating database."});
+    }
+});
+
+let server = app.listen(port, async () => {
+    console.log("Express Server Running on port :" + port);
+    await sequelize.authenticate();
+    console.log("Connected to Database.");
 })
